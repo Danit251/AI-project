5cea1769872c9f6a53a9c4e5b70823b36766fdb1
Danit Yshaayahu <danity@cs.huji.ac.il>
2018-07-11 17:36:28 +0300

    more texts for corpus

--- corpus/gutenberg.py
+++ corpus/gutenberg.py
@@ -3,24 +3,28 @@ from nltk.corpus import PlaintextCorpusReader
 
 # ['austen-emma.txt', 'austen-persuasion.txt', 'austen-sense.txt', 'bible-kjv.txt', 'blake-poems.txt', 'bryant-stories.txt', 'burgess-busterbrown.txt', 'carroll-alice.txt', 'chesterton-ball.txt', 'chesterton-brown.txt', 'chesterton-thursday.txt', 'edgeworth-parents.txt', 'melville-moby_dick.txt', 'milton-paradise.txt', 'shakespeare-caesar.txt', 'shakespeare-hamlet.txt', 'shakespeare-macbeth.txt', 'whitman-leaves.txt']
 
-f = open("Metadata", "w+")
+f = open("data/Melville-Romance/Metadata", "w+")
 
 n = gutenberg.fileids()
 # print(n)
+corpus = PlaintextCorpusReader("", 'Melville-Romance')
 count = 0
-for sen in gutenberg.sents("austen-emma.txt"):
+for sen in corpus.sents("Melville-Romance"):
+    # if 'Book' in sen:
+    # print(sen)
     if "CHAPTER" in sen:
         f.close()
-        f = open("austen-emma_" + str(count) + ".txt", "w+")
+        f = open("data/Melville-Romance/Melville-Romance_" + str(count) + ".txt", "w+")
         count += 1
     else:
         f.write(" ".join(sen) + " ")
 f.close()
 
-austin_corpus = PlaintextCorpusReader("", '.*\.txt')
-names = austin_corpus.fileids()
+# austin_corpus = PlaintextCorpusReader("data/austen-persuasion", '.*\.txt')
+# austin_corpus = PlaintextCorpusReader("", 'Melville-Romance')
+# names = austin_corpus.fileids()
 # print(names)
-for name in names:
-    print(name)
-    for sen in austin_corpus.sents(name):
-        print(sen)
+# for name in names:
+#     print(name)
+#     for sen in austin_corpus.sents(name):
+#         print(sen)
