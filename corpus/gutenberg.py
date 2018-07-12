bac2b770c86deeeee2def394d66a15ea5b3ecdf9
danity251 <danity251@gmail.com>
2018-07-12 14:51:01 +0300

    4 authres with 3 books each and all texts

--- corpus/gutenberg.py
+++ corpus/gutenberg.py
@@ -1,23 +1,29 @@
 from nltk.corpus import gutenberg
 from nltk.corpus import PlaintextCorpusReader
-
+import sys
+# import nltk
+# nltk.download()
 # ['austen-emma.txt', 'austen-persuasion.txt', 'austen-sense.txt', 'bible-kjv.txt', 'blake-poems.txt', 'bryant-stories.txt', 'burgess-busterbrown.txt', 'carroll-alice.txt', 'chesterton-ball.txt', 'chesterton-brown.txt', 'chesterton-thursday.txt', 'edgeworth-parents.txt', 'melville-moby_dick.txt', 'milton-paradise.txt', 'shakespeare-caesar.txt', 'shakespeare-hamlet.txt', 'shakespeare-macbeth.txt', 'whitman-leaves.txt']
-
-f = open("data/Melville-Romance/Metadata", "w+")
+# print("St?cker".encode(sys.stdout.encoding, errors='replace'))
+# print(u"St?cker")
+f = open("data/bronte/Bronte-Villette/Metadata", "w+")
 
 n = gutenberg.fileids()
 # print(n)
-corpus = PlaintextCorpusReader("", 'Melville-Romance')
+corpus = PlaintextCorpusReader("", 'Bronte-Villette')
 count = 0
-for sen in corpus.sents("Melville-Romance"):
+for sen in corpus.sents("Bronte-Villette"):
     # if 'Book' in sen:
     # print(sen)
-    if "CHAPTER" in sen:
+    if "Chapter" in sen:
         f.close()
-        f = open("data/Melville-Romance/Melville-Romance_" + str(count) + ".txt", "w+")
+        f = open("data/bronte/Bronte-Villette/Bronte-Villette_" + str(count) + ".txt", "w+")
         count += 1
     else:
-        f.write(" ".join(sen) + " ")
+        s = " ".join(sen) + " "
+        # e = s.encode('utf-8', errors='replace')
+        # print(s)
+        f.write(s)
 f.close()
 
 # austin_corpus = PlaintextCorpusReader("data/austen-persuasion", '.*\.txt')
