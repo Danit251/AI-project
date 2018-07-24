1bc90a2784bddf6794150da387e184e38cb53058
vardit <vardit.arkash@mail.huji.ac.il>
2018-07-24 19:02:06 +0300

    fixed num_of_short_words & average_sentence_length runtime

--- word_specific_features.py
+++ word_specific_features.py
@@ -4,11 +4,30 @@ import nltk
 nltk.download('stopwords')
 
 def calculate_words_feature_vector(text):
-    parse_text(text)
-    vector = [num_of_short_words()]
-    vector.append(average_word_length())
-    vector.append(average_sentence_length_by_words(text))
-    vector.append(average_sentence_length_by_chars())
+    # parse_text(text)
+    # vector = [num_of_short_words()]
+    # vector.append(average_word_length())
+    # vector.append(average_sentence_length_by_words(text))
+    # vector.append(average_sentence_length_by_chars())
+    # print(text)
+    text_without_punc = text
+    signs = set(string.punctuation)
+    signs.remove('.')
+    for sign in signs:
+        text_without_punc =  text_without_punc.replace(sign, "")
+        # text_without_punc =  text_without_punc.replace(" " + sign + " ", "")
+        # text_without_punc =  text_without_punc.replace(" " + sign, "")
+
+    text_without_dot =  text_without_punc.replace('.', "")
+    words = text_without_dot.split(" ")
+    # words_num = len(words)
+    # sents_num = text_without_punc.count('.')
+    # vector = [words_num + sents_num]
+
+    vector = [average_sentence_length_by_words(text_without_punc, words)]
+    # , average_sentence_length_by_chars(text_without_punc)]
+    # vector = [num_of_short_words(words), average_word_length(words)]
+    # vector = [num_function_words(text)]
     return vector
 
 
@@ -28,38 +47,56 @@ def parse_text(text):
 
 
 
-def num_of_short_words():
+def num_of_short_words(words):
     if not words:
         return 0
-    return short_words_count / len(words)
+    short_words = [x for x in words if len(x) < 3]
+    return len(short_words) / len(words)
 
 
-def average_word_length():
+def average_word_length(words):
     if not words:
         return 0
-    return total_words_length / len(words)
+    return sum(len(word) for word in words) / len(words)
 
 
-def average_sentence_length_by_words(text):
-    sentences = text.split(" . ")
-    len_by_words = 0
-    global len_by_chars, num_sents
-    len_by_chars = 0
-    num_sents = 0
-    for sen in sentences:
-        for sign in set(string.punctuation):
-            sen = sen.replace(sign+" ", "")
-        if sen:
-            num_sents += 1
-            words_in_sen = sen.split(' ')
-            len_by_words += len(words_in_sen)
-            len_by_chars += len(sen)
+def average_sentence_length_by_words(text, words):
+    # sentences = text.split(" . ")
+    # len_by_words = 0
+    # global len_by_chars, num_sents
+    # len_by_chars = 0
+    # num_sents = 0
+    # for sen in sentences:
+    #     for sign in set(string.punctuation):
+    #         sen = sen.replace(sign+" ", "")
+    #     if sen:
+    #         num_sents += 1
+    #         words_in_sen = sen.split(' ')
+    #         len_by_words += len(words_in_sen)
+    #         len_by_chars += len(sen)
 
-    return len_by_words/num_sents
+    # text_without_punc = text
+    # # signs = set(string.punctuation)
+    # # signs.remove('.')
+    # # for sign in signs:
+    # #     text_without_punc =  text_without_punc.replace(sign, "")
 
+    len_by_words = len(words)
 
-def average_sentence_length_by_chars():
-    return len_by_chars/num_sents
+    global sents_num
+    sents_num = text.count('.')
+
+    if sents_num == 0:
+        return 0
+    return len_by_words/sents_num
+
+def average_sentence_length_by_chars(text):
+    text_without_punc = text
+    text_without_punc =  text_without_punc.replace(' ', "")
+    len_by_chars = len(text_without_punc)
+    if sents_num == 0:
+        return 0
+    return len_by_chars/sents_num
 
 
 def occurance_of_words(text):
@@ -89,16 +126,18 @@ def occurance_of_words(text):
 
 def num_function_words(text):
     stp = nltk.corpus.stopwords.words('english')
-    # filtered_text = [w for w in text if w in stp]
-    print(stp)
+    words = text.split(" ")
+    filtered_text = set(stp) & set(words)
+    # filtered_text = [w for w in words if w in stp]
+    # print(filtered_text)
+    return len(filtered_text)
 
 
+# with open("corpus/data/austen/austen-sense/austen-sense_8.txt", 'r') as file:
+#     text = file.read()
 
-with open("corpus/data/austen/austen-sense/austen-sense_8.txt", 'r') as file:
-    text = file.read()
-
-text = "this is a test sentence that is hopefully long enough to be helpful. a a"
+# text = "this is a test sentence that is hopefully long enough to be helpful. a a"
 
 # print(calculate_words_feature_vector(text))
 # print(occurance_of_words(text))
-num_function_words(text)
+# num_function_words(text)
