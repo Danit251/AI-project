c27bb432fc39a9b3039aa827535cd6f4409734b0
nogastern <noga.stern@mail.huji.ac.il>
2018-08-01 19:06:35 +0300

    finished bronte's and melville's books

--- dependency_parse.py
+++ dependency_parse.py
@@ -5,19 +5,10 @@ import nltk.data
 nlp = StanfordCoreNLP("http://localhost:9000")  # this requires a server to run
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
-def debug_jane_eyre():
-    author = "bronte"
-    book = "Bronte-Jane-Eyre"
-    filename = "Bronte-Jane-Eyre_32.txt"
-    with open("corpus/data/" + author + "/" + book + "/" + filename, 'r', encoding='utf-8', errors='ignore') as read_file:
-        text = read_file.read()
-    with open("corpus/parsed_data/" + author + "/" + book + "/" + filename, 'w', encoding='utf-8', errors='ignore') as write_file:
-        write_to_file(write_file, text)
-
 
 def create_files():
     for author in os.listdir("corpus/data"):
-        if author.startswith(".") or author == "bronte":
+        if author.startswith(".") or author == "bronte" or author == "melville":
             continue
         for book in os.listdir("corpus/data/" + author):
             if book.startswith("."):
@@ -44,10 +35,9 @@ def write_to_file(write_file, text):
             res = nlp.annotate(sent, properties={'annotators':'parse', 'outputFormat': 'json'})
             write_file.write(res['sentences'][0]['parse'])
             write_file.write("\n~~~~~~\n")
-        except TypeError:
+        except TypeError: # if the sentence is too long the StanfordCoreNLP tool throws an exception
             write_file.write("no parsing")
             write_file.write("\n~~~~~~\n")
 
 
 create_files()
-# debug_jane_eyre()
