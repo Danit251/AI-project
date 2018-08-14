1cf9de1ab2adf3885ba738ba03abbfe2b353d2d8
danity251 <danity251@gmail.com>
2018-08-14 21:12:32 +0300

    again push to run's files

--- run_nn.py
+++ run_nn.py
@@ -1,18 +1,8 @@
 import calculate_features
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
 import util
-
-
-# parameters = {
-#     'n_neighbors': [3, 5, 7, 17, 35, 100],
-#     'algorithm': ['auto', 'ball_tree', 'kd_tree'],
-#     'leaf_size': [50, 10, 75],
-#     'p': [1, 3],
-#     'n_jobs': [2, 3]
-# }
 
 
 def run(test_ratio, data, split_by_book=False, repeat=False):
@@ -22,7 +12,10 @@ def run(test_ratio, data, split_by_book=False, repeat=False):
         num_of_runs = util.REPEAT_ITERATION['NN']
     else:
         num_of_runs = 1
+    # Calculates NN number of times
     while run_count < num_of_runs:
+
+        # Splits data to train and test
         if split_by_book:
             training_data, test_data = calculate_features.split_train_test(data)
             X_train = training_data[:, 0]
@@ -33,15 +26,22 @@ def run(test_ratio, data, split_by_book=False, repeat=False):
             X = data[:, 0]
             y = data[:, 1]
             X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_ratio)
+
         clf = KNeighborsClassifier(n_neighbors=5, algorithm='auto', leaf_size=50, n_jobs=2, p=1)
+
         # train
         clf.fit(np.ndarray.tolist(X_train), np.ndarray.tolist(y_train))
+
         # test
         score = clf.score(np.ndarray.tolist(X_test), np.ndarray.tolist(y_test))
+
         if repeat:
             print('Iteration {} of Nearest Neighbors resulted score of: {}\n'.format(run_count, score))
+
         score_sum += score
         run_count += 1
+
     if repeat:
         return clf, score_sum / num_of_runs
+
     return clf, score
--- run_rf.py
+++ run_rf.py
@@ -5,29 +5,6 @@ from sklearn.ensemble import RandomForestClassifier
 import util
 
 
-# 'min_weight_fraction_leaf',
-# 'class_weight',
-# 'min_samples_split',
-# 'max_depth',
-# 'criterion',
-# 'min_samples_leaf',
-# 'random_state',
-# 'max_features',
-# 'min_impurity_split',
-# 'max_leaf_nodes',
-# 'splitter',
-# 'presort'
-
-
-parameters = {
-    'max_depth': range(1,8),
-    'max_features': range(20, 40),
-    'n_estimators': range(20,30),
-    # 'min_samples_split': range(1,7),
-    # 'min_samples_leaf': range(1,5)
-}
-
-
 def run(test_ratio, data, split_by_book=False, repeat=False):
     run_count = 0
     score_sum = 0
@@ -35,7 +12,10 @@ def run(test_ratio, data, split_by_book=False, repeat=False):
         num_of_runs = util.REPEAT_ITERATION['RF']
     else:
         num_of_runs = 1
+    # Calculates RF number of times
     while run_count < num_of_runs:
+
+        # Splits the data to train and test
         if split_by_book:
             training_data, test_data = calculate_features.split_train_test(data)
             X_train = training_data[:, 0]
@@ -46,15 +26,22 @@ def run(test_ratio, data, split_by_book=False, repeat=False):
             X = data[:, 0]
             y = data[:, 1]
             X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_ratio)
+
         clf = RandomForestClassifier(max_depth=6, max_features=21, min_samples_leaf=2, n_estimators=28, random_state=0)
+
         # train
         clf.fit(np.ndarray.tolist(X_train), np.ndarray.tolist(y_train))
+
         # test
         score = clf.score(np.ndarray.tolist(X_test), np.ndarray.tolist(y_test))
+
         if repeat:
             print('Iteration {} of Random Forest resulted score of: {}\n'.format(run_count, score))
+
         score_sum += score
         run_count += 1
+
     if repeat:
         return clf, score_sum / num_of_runs
+
     return clf, score
