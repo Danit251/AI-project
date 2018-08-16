169104b7e01fe6f9209362303044a008e719d7f9
roi1255 <roi.shtivi@mail.huji.ac.il>
2018-08-16 11:21:27 +0300

    Stoker-Dracula and a bit on repord PDF

--- corpus/gutenberg.py
+++ corpus/gutenberg.py
@@ -1,23 +1,23 @@
 from nltk.corpus import gutenberg
 from nltk.corpus import PlaintextCorpusReader
 import sys
-# import nltk
+import nltk
 # nltk.download()
 # ['austen-emma.txt', 'austen-persuasion.txt', 'austen-sense.txt', 'bible-kjv.txt', 'blake-poems.txt', 'bryant-stories.txt', 'burgess-busterbrown.txt', 'carroll-alice.txt', 'chesterton-ball.txt', 'chesterton-brown.txt', 'chesterton-thursday.txt', 'edgeworth-parents.txt', 'melville-moby_dick.txt', 'milton-paradise.txt', 'shakespeare-caesar.txt', 'shakespeare-hamlet.txt', 'shakespeare-macbeth.txt', 'whitman-leaves.txt']
 # print("St?cker".encode(sys.stdout.encoding, errors='replace'))
 # print(u"St?cker")
-f = open("data/bronte/Bronte-Villette/Metadata", "w+")
+f = open("data/stoker/Stoker-Dracula/Metadata", "w+")
 
-n = gutenberg.fileids()
+# n = gutenberg.fileids()
 # print(n)
-corpus = PlaintextCorpusReader("", 'Bronte-Villette')
+corpus = PlaintextCorpusReader("", 'Stoker-Dracula')
 count = 0
-for sen in corpus.sents("Bronte-Villette"):
+for sen in corpus.sents("Stoker-Dracula"):
     # if 'Book' in sen:
     # print(sen)
-    if "Chapter" in sen:
+    if "CHAPTER" in sen:
         f.close()
-        f = open("data/bronte/Bronte-Villette/Bronte-Villette_" + str(count) + ".txt", "w+")
+        f = open("data/stoker/Stoker-Dracula/Stoker-Dracula_" + str(count) + ".txt", "w+")
         count += 1
     else:
         s = " ".join(sen) + " "
